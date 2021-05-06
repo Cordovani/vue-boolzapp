933148107e14a3194831e05de8e1b2cfe8e352c8
Cordovani <lor.cordovani@gmail.com>
2021-05-06 17:29:54 +0200

    funzione per mandare il messaggio

--- JS/contatti.js
+++ JS/contatti.js
@@ -81,4 +81,4 @@ const globalUsersList = [
             }
         ],
     },
-];
+]
--- JS/script.js
+++ JS/script.js
@@ -4,16 +4,36 @@ var app = new Vue({
     el: '#main',
     data: {
         usersList: globalUsersList,
-        selectedUser: {}
+        selectedUser: {},
+        // input con il v-model
+        newText: ""
     },
     methods: {
         // restituisce il path per cambiare immagine
         getAvatarPath(avatarID){
             return `img/avatar${avatarID}.jpg`;
         },
+
         // per selezionare la conversazione
         onUserClick(user){
             this.selectedUser = user;
-        }  
+        }, 
+
+        // formatto l'orario con moment.js
+        formattedTime(data) {
+            return moment(data, "DD/MM/YYYY HH:mm:ss").format("HH:mm");
+        },
+
+        // creo il nuovo messaggio e lo pusho nell'array dell'utente selezionato
+        // moment() restituisce la data corrente
+        sendMessage() {
+            const newMessage = {
+                date: moment().format("HH:mm"),
+                text: this.newText,
+                status: "sent"
+            }
+
+            this.selectedUser.messages.push(newMessage);
+        }
     }
 })
