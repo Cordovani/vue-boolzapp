288f482f9640198a1ae3a833943f34d6f8a93dd9
Cordovani <lor.cordovani@gmail.com>
2021-05-07 14:59:01 +0200

    fix formato data e messaggio di risposta

--- JS/script.js
+++ JS/script.js
@@ -10,14 +10,14 @@ var app = new Vue({
     },
     methods: {
         // restituisce il path per cambiare immagine
-        getAvatarPath(avatarID){
+        getAvatarPath(avatarID) {
             return `img/avatar${avatarID}.jpg`;
         },
 
         // per selezionare la conversazione
-        onUserClick(user){
+        onUserClick(user) {
             this.selectedUser = user;
-        }, 
+        },
 
         // formatto l'orario con moment.js
         formattedTime(data) {
@@ -27,13 +27,32 @@ var app = new Vue({
         // creo il nuovo messaggio e lo pusho nell'array dell'utente selezionato
         // moment() restituisce la data corrente
         sendMessage() {
+
             const newMessage = {
-                date: moment().format("HH:mm"),
+                date: moment().format("DD/MM/YYYY HH:mm:ss"),
                 text: this.newText,
                 status: "sent"
             }
 
             this.selectedUser.messages.push(newMessage);
-        }
+
+            // triggera al sendMessage()
+            // parte il timer di 1 secondo --> setTimeOut 
+            // ritorna il messaggio OK!
+            // pushare il messaggio nei ricevuti del selectedUser
+            // arrow function per accedere al this
+            setTimeout(() => {
+                const okRespond = {
+                    // il messaggio deve formattare la data completa
+                    date: moment().format("DD/MM/YYYY HH:mm:ss"),
+                    text: 'OK! Boomer',
+                    status: 'received'
+                }
+
+                this.selectedUser.messages.push(okRespond);
+
+            },1000);
+
+        },
     }
 })
